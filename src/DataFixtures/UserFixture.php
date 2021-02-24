1e48ab38b884b68492afaddc8fb9f7d07eb16b37
LennyLouis <5RUEdebussy>
2021-02-24 10:33:02 +0100

    qualques changement, a faire: l'upload d'avatar

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,26 +2,42 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\UserOld;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
 
         $faker = Faker\Factory::create();
 
         for($i = 0; $i<10; $i++) {
-            $user = new UserOld();
+            $files = glob('/tmp' . '/*.*'); // TODO : Changer ca et faire un upload de fichier
+            $file = array_rand($files);
+            $path = $files[$file];
+            $type = pathinfo($path, PATHINFO_EXTENSION);
+            $data = file_get_contents($path);
+            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+            $user = new User();
             $user->setFirstName($faker->firstName)
                 ->setLastName($faker->lastName)
-                ->setBirthdate($faker->date())
-                ->setAvatar($faker->imageUrl(150, 150))
+                ->setBirthdate(new \DateTime())
+                ->setUsername($faker->userName)
+                ->setAvatar($base64)
                 ->setMail($faker->email)
-                ->setPassword("")
+                ->setPassword($this->encoder->encodePassword($user, $faker->password))
                 ->setAddress($faker->address)
                 ->setGender("")
                 ->setAccountStatus("pending")
